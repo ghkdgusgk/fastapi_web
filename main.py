62e10930ca20b6a518fa2396ac47875f4edd35ac
ghkdgusgk <ghkdgusgk526>
2024-01-31 13:29:56 +0900

    index.html rendering

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(name="index.html", request=request)
+
+
+@app.get("/hello")
 async def root():
-    return {"message": "Helloasdsad H"}
+    return {"message": "안녕! 반가워!"}
